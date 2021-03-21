fcb9bdb67f9bf407a5b425378b72ec5f3c8b97dd
lanta <lantaWeb@inbox.lv>
2021-03-22 01:16:30 +0200

    Updates

---
+++ precache-manifest.ec8c141fcab39b418eb22f7630137a6e.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "da22d243a50ddfc14b4c",
+    "url": "lanta.lv/static/css/main.bde7215b.chunk.css"
+  },
+  {
+    "revision": "da22d243a50ddfc14b4c",
+    "url": "lanta.lv/static/js/main.da22d243.chunk.js"
+  },
+  {
+    "revision": "25027f31a685e5b97700",
+    "url": "lanta.lv/static/js/runtime~main.25027f31.js"
+  },
+  {
+    "revision": "281a03766082581d0335",
+    "url": "lanta.lv/static/js/2.281a0376.chunk.js"
+  },
+  {
+    "revision": "e4d371034e6698ceec645f9defa8ea7b",
+    "url": "lanta.lv/static/media/discount.e4d37103.png"
+  },
+  {
+    "revision": "1b1de90bdbca8e202259309288222080",
+    "url": "lanta.lv/static/media/sales.1b1de90b.jpg"
+  },
+  {
+    "revision": "58d4198a6af960443d0629bba769c7a4",
+    "url": "lanta.lv/static/media/1_edited.58d4198a.jpg"
+  },
+  {
+    "revision": "061a5a5e0a1322e09f20235703d611d8",
+    "url": "lanta.lv/static/media/services_hairdrsser2.061a5a5e.jpg"
+  },
+  {
+    "revision": "2a3dc791ecf0a61cf57046fc845e9b1b",
+    "url": "lanta.lv/static/media/services_manicure2.2a3dc791.jpg"
+  },
+  {
+    "revision": "c132092107a7de1302604a93e7b5e332",
+    "url": "lanta.lv/static/media/services_bodyCare2.c1320921.jpg"
+  },
+  {
+    "revision": "6958ad70ad7f858103358f89a9fef111",
+    "url": "lanta.lv/static/media/services_solarium2.6958ad70.jpg"
+  },
+  {
+    "revision": "b71cfeacffde68b2e2b6e079bba442ec",
+    "url": "lanta.lv/static/media/2_edited.b71cfeac.jpg"
+  },
+  {
+    "revision": "e7b90582c17e2bb0389c7275b64e2813",
+    "url": "lanta.lv/static/media/11_edited.e7b90582.jpg"
+  },
+  {
+    "revision": "702d8be9725e86036d1ca8b2cbd0ce74",
+    "url": "lanta.lv/static/media/background2.702d8be9.png"
+  },
+  {
+    "revision": "5a061c75c30230501ce721b436ffeaf6",
+    "url": "lanta.lv/static/media/10valkas_edited.5a061c75.jpg"
+  },
+  {
+    "revision": "58ac5330c1602b6479df2a263efd92b8",
+    "url": "lanta.lv/static/media/15ditton.58ac5330.jpg"
+  },
+  {
+    "revision": "d128a4a3578c10002292390452533a5a",
+    "url": "lanta.lv/static/media/15ditton_edited.d128a4a3.jpg"
+  },
+  {
+    "revision": "eecb6af37d2634cd11e672b21375d7f2",
+    "url": "lanta.lv/static/media/19valkas_edited.eecb6af3.jpg"
+  },
+  {
+    "revision": "a97854e5f6e7d64b686b66de1cdefad0",
+    "url": "lanta.lv/static/media/4ditton_edited.a97854e5.jpg"
+  },
+  {
+    "revision": "5507526923e8f09e440cce12950c5df1",
+    "url": "lanta.lv/static/media/5valkas_edited.55075269.jpg"
+  },
+  {
+    "revision": "2427b1ff50a8b69e8d1600beced60a7f",
+    "url": "lanta.lv/static/media/6valkas_edited.2427b1ff.jpg"
+  },
+  {
+    "revision": "8c8b0b5e98f7f31e61ecb43c1bcff9c7",
+    "url": "lanta.lv/static/media/9ditton_edited.8c8b0b5e.jpg"
+  },
+  {
+    "revision": "44891b7d422095c5c405853465ad0a2f",
+    "url": "lanta.lv/static/media/alfa0logo.44891b7d.jpg"
+  },
+  {
+    "revision": "dccac6b6b32b68d413319264f1c0ea04",
+    "url": "lanta.lv/static/media/alfa1.dccac6b6.jpg"
+  },
+  {
+    "revision": "c6facbf802d609d3d64f790caf72dae9",
+    "url": "lanta.lv/static/media/alfa2.c6facbf8.jpg"
+  },
+  {
+    "revision": "23658510e150db24c462a80bbb0a8db0",
+    "url": "lanta.lv/static/media/alfa3.23658510.png"
+  },
+  {
+    "revision": "0edd52eca2c04bd967bdbbf18aa41925",
+    "url": "lanta.lv/static/media/loreal0logo.0edd52ec.jpg"
+  },
+  {
+    "revision": "7471a9b60960c6177f8bb78f953c621f",
+    "url": "lanta.lv/static/media/loreal1.7471a9b6.jpg"
+  },
+  {
+    "revision": "b7141d09268e3525e104dd1f74ee39cf",
+    "url": "lanta.lv/static/media/loreal2.b7141d09.jpg"
+  },
+  {
+    "revision": "899378790881ddc0f2718bf9e838a144",
+    "url": "lanta.lv/static/media/loreal3.89937879.jpg"
+  },
+  {
+    "revision": "faaf8201ab779b8088a85384f8fef454",
+    "url": "lanta.lv/static/media/loreal4.faaf8201.jpg"
+  },
+  {
+    "revision": "057c02727346deed8a94c9e3868418dc",
+    "url": "lanta.lv/static/media/oasis1.057c0272.jpg"
+  },
+  {
+    "revision": "fa81a776e46e0f41944fc632bee0de0b",
+    "url": "lanta.lv/static/media/oasis2.fa81a776.png"
+  },
+  {
+    "revision": "f3b43df6c4f968257831c53c8b266f60",
+    "url": "lanta.lv/static/media/oasis3.f3b43df6.jpg"
+  },
+  {
+    "revision": "71dd9d155d0e52e608ba920398e2a046",
+    "url": "lanta.lv/static/media/revlon0logo.71dd9d15.jpg"
+  },
+  {
+    "revision": "5a2d6f928234fe593570012912800f0d",
+    "url": "lanta.lv/static/media/revlon1.5a2d6f92.jpg"
+  },
+  {
+    "revision": "b2d7c7b686713f72299823faf3b702f8",
+    "url": "lanta.lv/static/media/revlon2.b2d7c7b6.png"
+  },
+  {
+    "revision": "9f476c73d69541358b68e9a06c32118b",
+    "url": "lanta.lv/static/media/revlon3.9f476c73.jpg"
+  },
+  {
+    "revision": "76f55b85d71ca3b00f0a6f726a8368d2",
+    "url": "lanta.lv/static/media/revlon4.76f55b85.jpg"
+  },
+  {
+    "revision": "221b57d4cf99a1f469ef4332ac70e566",
+    "url": "lanta.lv/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d1317a36afc5f237d598643c642a6f2.js"
+  "lanta.lv/precache-manifest.ec8c141fcab39b418eb22f7630137a6e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("lanta.lv/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
